496021cee73c832d983a4f0664e6c844c084d90d
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-10-07 08:34:01 +0530

    Update GenerateManual.py

--- GenerateManual.py
+++ GenerateManual.py
@@ -4,7 +4,7 @@ from GetPairNumberFromColour import get_pair_number_from_color
 def color_pair_to_string(major_color, minor_color):
     return f'{major_color} {minor_color}'
 
-def generate_reference_manual():
+def generate_manual():
     """Generates a reference manual of color pairs."""    
     for pair_number in range(1, 26):
         major, minor = get_color_from_pair_number(pair_number)
